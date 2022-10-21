bc8a83a6d9c0f808e79960ad33ede3badf2036d8
Julio <jcesarqalonso@gmail.com>
2022-10-21 10:46:38 -0300

    first commit

---
+++ src/main/java/py/com/lincoln/soap/webservice/client/CountryClient.java
@@ -0,0 +1,32 @@
+package py.com.lincoln.soap.webservice.client;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.client.core.SoapActionCallback;
+
+import py.com.lincoln.soap.webservice.client.GetCountryRequest;
+import py.com.lincoln.soap.webservice.client.GetCountryResponse;
+
+
+public class CountryClient extends WebServiceGatewaySupport {
+
+    private static final Logger log = LoggerFactory.getLogger(CountryClient.class);
+
+    public GetCountryResponse getCountry(String country) {
+
+        GetCountryRequest request = new GetCountryRequest();
+        request.setName(country);
+
+        log.info("Requesting location for " + country);
+
+        GetCountryResponse response = (GetCountryResponse) getWebServiceTemplate()
+                .marshalSendAndReceive("http://localhost:8080/ws/countries", request,
+                        new SoapActionCallback(
+                                "http://spring.io/guides/gs-producing-web-service/GetCountryRequest"));
+
+        return response;
+    }
+
+}
